36fbff8835d4043c38e4bd22bf1f3e9e15519c10
YuriyOrlov <navuchodonsr@yandex.ru>
2017-06-13 16:54:43 +0200

    Added rating retrieve from other site. Everything works fine, but there some problems with rating retrieval for some films. Needs further investigation.

--- cinemas.py
+++ cinemas.py
@@ -15,22 +15,22 @@ def parse_page(raw_html):
     return BeautifulSoup(raw_html.content, 'lxml')
 
 
-def fetch_movie_info(raw_info):
+def fetch_afisha_movie_info(raw_info):
     parsed_data = raw_info.find_all('h3', class_=re.compile('usetags'))
     movie_names = [movie_name.text for movie_name in parsed_data]
     movie_links = [movie_link.a.get('href') for movie_link in parsed_data]
     movie_id_number = re.compile(r'[0-9]+')
     movies_id_list = [movie_id_number.search(link).group(0) for link in movie_links]
-    movie_links = ['https://www.afisha.ru/msk/schedule_cinema_product/{}'.format(movie_id)
-                   for movie_id in movies_id_list]
-    number_of_movie_theaters = parallel_page_parsing(movie_links)
-    return zip(movie_names, number_of_movie_theaters)
+    afisha_movie_url = 'https://www.afisha.ru/msk/schedule_cinema_product/'
+    movie_links = ['{}{}'.format(afisha_movie_url, movie_id) for movie_id in movies_id_list]
+    number_of_movie_theaters = parallel_page_parsing(get_number_of_movie_theaters, movie_links)
+    return movie_names, number_of_movie_theaters
 
 
-def parallel_page_parsing(movie_links):
+def parallel_page_parsing(func, movie_links):
     num_of_parallel_processes = cpu_count() * 2
     pool = Pool(num_of_parallel_processes)
-    return pool.map(get_number_of_movie_theaters, movie_links)
+    return pool.map(func, movie_links)
 
 
 def get_number_of_movie_theaters(movie_link):
@@ -39,12 +39,33 @@ def get_number_of_movie_theaters(movie_link):
     return len(parsed_movie_info_page.find_all('td', class_=re.compile('b-td-item')))
 
 
-def output_movies_to_console(movies):
-    pass
+def get_mean_rating(movie_link):
+    raw_data_page = fetch_page(movie_link)
+    parsed_movie_info_page = parse_page(raw_data_page)
+    try:
+        rating = parsed_movie_info_page.find('span', class_=re.compile('rating_ball')).text
+        rating = float(rating)
+    except:
+        rating = None
+    return rating
+
+
+def retrieve_kinopoisk_movie_info(movie_names):
+    kinopoisk_movie_url = 'https://www.kinopoisk.ru/index.php?first=yes&what=&kp_query='
+    movie_links = ['{}{}'.format(kinopoisk_movie_url, movie_name) for movie_name in movie_names]
+    return parallel_page_parsing(get_mean_rating, movie_links)
+
+
+def output_movies_to_console(parsed_page):
+    movie_names, number_of_movie_theaters = fetch_afisha_movie_info(parsed_page)
+    movie_ratings = retrieve_kinopoisk_movie_info(movie_names)
+    return zip(movie_names, number_of_movie_theaters, movie_ratings)
 
 
 if __name__ == '__main__':
     raw_movies_page = fetch_page('https://www.afisha.ru/msk/schedule_cinema/')
     parsed_page_w_movies_list = parse_page(raw_movies_page)
-    [print('{} -- > {}'.format(movie, number_of_theaters)) for movie, number_of_theaters in fetch_movie_info(parsed_page_w_movies_list)]
-    # output_movies_to_console(movie_names)
+    # movie_names, number_of_movie_theaters, movie_ratings = output_movies_to_console(parsed_page_w_movies_list)
+    for names, theaters, ratings in output_movies_to_console(parsed_page_w_movies_list):
+        print('Фильм --> Кол-во --> Рейтинг')
+        print('{} --> {} --> {}'.format(names, theaters, ratings))
